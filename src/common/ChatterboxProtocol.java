fec79453179241d8ecccf02f63a7a000bd766a79
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2018-03-21 15:32:41 -0400

    Updated the protocol to include last minute additions.

--- src/common/ChatterboxProtocol.java
+++ src/common/ChatterboxProtocol.java
@@ -6,9 +6,15 @@ package common;
  */
 public interface ChatterboxProtocol {
     /**
+     * Chatterbox clients and servers should always use this port to establish
+     * socket connections.
+     */
+    public static final int PORT = 6789;
+
+    /**
      * Used to separate tokens in protocol messages.
      */
-     String SEPARATOR = "::";
+    public static String SEPARATOR = "::";
 
     //
     // CLIENT MESSAGES - sent from client to server
@@ -18,31 +24,31 @@ public interface ChatterboxProtocol {
      * The protocol message sent from the client to the Chatterbox server to
      * initially connect to the chat room.
      */
-     String CONNECT = "connect";
+    public static String CONNECT = "connect";
 
     /**
      * The protocol message sent from the client to the Chatterbox server to
      * disconnect from the chat room.
      */
-     String DISCONNECT = "disconnect";
+    public static String DISCONNECT = "disconnect";
 
     /**
      * The protocol message sent from the client to the Chatterbox server to
      * send a message to the chat room.
      */
-     String SEND_CHAT = "send_chat";
+    public static String SEND_CHAT = "send_chat";
 
     /**
      * The protocol message sent from the client to the Chatterbox server to
      * send a whisper to another user in the chat room.
      */
-     String SEND_WHISPER = "send_whisper";
+    public static String SEND_WHISPER = "send_whisper";
 
     /**
      * The protocol message sent from the client to the Chatterbox server to
      * list the users currently connected to the chat room.
      */
-     String LIST_USERS = "list_users";
+    public static String LIST_USERS = "list_users";
 
     //
     // SERVER MESSAGES - sent from server to client
@@ -52,50 +58,57 @@ public interface ChatterboxProtocol {
      * The protocol message sent from the Chatterbox server to client to
      * indicate that the connection was successful.
      */
-     String CONNECTED = "connected";
+    public static String CONNECTED = "connected";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * indicate that the disconnection was successful.
      */
-     String DISCONNECTED = "disconnected";
+    public static String DISCONNECTED = "disconnected";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * indicate that the chat message was successfully received.
      */
-     String CHAT_RECEIVED = "chat_received";
+    public static String CHAT_RECEIVED = "chat_received";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * indicate that the whisper was successfully received.
      */
-     String WHISPER_RECEIVED = "whisper_received";
+    public static String WHISPER_RECEIVED = "whisper_received";
+
+    /**
+     * The protocol message indicating that a whisper has been successfully
+     * sent to its intended recipient; used as an acknowledgement from the
+     * server to sender that the message was delivered.
+     */
+    public static String WHISPER_SENT = "whisper_sent";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * respond to a request for a list of users.
      */
-     String USERS = "users";
+    public static String USERS = "users";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * notify the client that a new user has joined the chat room.
      */
-     String USER_JOINED = "user_joined";
+    public static String USER_JOINED = "user_joined";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * notify the client that a user has left the chat room.
      */
-     String USER_LEFT = "user_left";
+    public static String USER_LEFT = "user_left";
 
     /**
      * The protocol message sent from the Chatterbox server to client to
      * notify the client that an error occurred when the server received the
      * most recent request.
      */
-     String ERROR = "error";
+    public static String ERROR = "error";
 
     /**
      * The protocol message sent from the ChatterBoc server to the client to
@@ -103,5 +116,5 @@ public interface ChatterboxProtocol {
      * followed by a termination of the connection between the client and the
      * server.
      */
-     String FATAL_ERROR = "fatal_error";
+    public static String FATAL_ERROR = "fatal_error";
 }
